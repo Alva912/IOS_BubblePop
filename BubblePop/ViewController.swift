e646bf14e33bbe135fcade3945b423e4a2a9416f
Alva912 <lijahua912@gmail.com>
2021-01-17 13:58:10 +1100

    Create a new project

---
+++ BubblePop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BubblePop
+//
+//  Created by ljh on 17/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
